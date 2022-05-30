f42c82ab6c74c2b683e5654b610f546de1dad990
kay4yk13 <denis.klimovich.web@gmail.com>
2022-05-31 01:27:48 +0300

    glass and random lines and keyevents

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,12 +1,17 @@
 <template>
 	<div class="controls">
 		<h1>CONTROLS</h1>
-		<button @click="btnpress">GAMECHANGER</button>
-		<button @click="random">RANDOM</button>
+		<Keypress key-event="keyup" :key-code="13" @success="btnpress" />
+		<Keypress key-event="keyup" :key-code="32" @success="random" />
+		<!-- <button @click="btnpress">GAMECHANGER</button>
+		<button @click="random">RANDOM</button> -->
 	</div>
 </template>
 <script>
 export default {
+	components: {
+		Keypress: () => import("vue-keypress"),
+	},
 	methods: {
 		btnpress() {
 			this.$store.dispatch("Togle", "game");
